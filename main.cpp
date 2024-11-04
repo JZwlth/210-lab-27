f0254bd3e1051510085a751608bb44752e7e3362
JZwlth <wkjazy87@gmail.com>
2024-11-03 20:26:20 -0800

    update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void displayVillagers(const map<string, tuple<int, string, string>>& villagers) 
 
 }
 
+
 int main() {
     map<string, tuple<int, string, string>> villagers;
     int choice;
@@ -17,10 +18,27 @@ int main() {
         cout << "1. Add Villager\n"
              << "2. Delete Villager\n"
              << "3. Increase Friendship\n"
-
+             << "4. Decrease Friendship\n"
+             << "5. Search for Villager\n"
+             << "6. Exit\n"
+             << "Enter choice: ";
         cin >> choice;
         cin.ignore();  // Clear the input buffer
+switch (choice) {
+            case 1: {
+                // Add Villager
+                string name, species, catchphrase;
+                int friendshipLevel;
 
-    };
+                cout << "Species: ";
+                getline(cin, species);
+                cout << "Catchphrase: ";
+                getline(cin, catchphrase);
+
+                villagers[name] = make_tuple(friendshipLevel, species, catchphrase);
+                cout << name << " added.\n";
+                break;
+            }
+            case 2: {
     return 0;
 }
